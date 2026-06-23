9ce8a84cd4fd5dc293a1855c093d841c5f617ab1
Rafael Valverde <rafaverde@msn.com>
2026-06-23 17:50:54 -0300

    feat: enhance AgendaCard styling and update AgendaSection layout for improved responsiveness

--- src/components/sections/agenda.tsx
+++ src/components/sections/agenda.tsx
@@ -12,25 +12,25 @@ export default function AgendaSection() {
     <section id="agenda" className="scroll-m-14 bg-[url('/grunge-bg.webp')]">
       <div className="container mx-auto flex flex-col gap-8 px-4 py-14">
         <div className="space-y-3">
-          <h2 className="font-heading text-4xl text-slate-400 uppercase">
+          <h2 className="flex items-center justify-between gap-2 font-heading text-4xl text-slate-400 uppercase">
             Agenda
           </h2>
         </div>
 
         <Carousel opts={{ align: 'start' }}>
-          <CarouselContent className="-ml-8">
+          <CarouselContent className="-ml-8 max-w-[80%] md:max-w-full">
             {Array.from({ length: 6 }).map((_, index) => (
               <CarouselItem
                 // biome-ignore lint/suspicious/noArrayIndexKey: <Temporary>
                 key={index}
-                className="basis-1/2 pl-8 md:basis-1/3 lg:basis-1/4"
+                className="basis pl-8 md:basis-1/3 lg:basis-1/4"
               >
                 <AgendaCard />
               </CarouselItem>
             ))}
           </CarouselContent>
-          <CarouselPrevious className="-left-4 hidden cursor-pointer bg-violet-300/40! md:inline-flex" />
-          <CarouselNext className="-right-4 hidden cursor-pointer bg-violet-300/40! md:inline-flex" />
+          <CarouselPrevious className="-left-2 md:-left-4 cursor-pointer bg-violet-300/40! md:inline-flex" />
+          <CarouselNext className="-right-2 md:-right-4 cursor-pointer bg-violet-300/40! md:inline-flex" />
         </Carousel>
       </div>
     </section>
