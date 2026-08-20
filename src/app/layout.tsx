6e75796bc8b9b6ae0402603a636eb3bc45ff1c87
Rafael Valverde <rafaverde@msn.com>
2026-08-20 09:07:33 -0300

    refactor: enhance metadata in layout component with keywords and Open Graph properties

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,24 @@ export const metadata: Metadata = {
   title: 'Banda Headpuppets',
   description:
     'Banda de rock clássico e heavy metal em Natal, Rio Grande do Norte',
+  keywords:
+    'banda natal, banda natal rn, banda para eventos, eventos em natal rn, banda rock rn, banda eventos rn, banda eventos moto, motoclube, banda rock para bar em natal, banda, rock, cover, natal, rn',
+  openGraph: {
+    title: 'Banda Headpuppets',
+    description:
+      'Banda de rock clássico e heavy metal em Natal, Rio Grande do Norte',
+    url: process.env.NEXT_PUBLIC_BASE_URL || 'https://www.headpuppets.com.br',
+    siteName: 'Banda Headpuppets',
+    images: [
+      {
+        url: '/bg-banda-grunge-halloween.webp',
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: 'pt_BR',
+    type: 'website',
+  },
 }
 
 export default async function RootLayout({
